a1265ee82c8ace5baa208069cba00dba0c5a58d4
Shiv10032005 <sphoniex9999@gmail.com>
2026-06-10 22:38:17 +0530

    fix: force bundle ejs in serverless

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,5 @@
 const serverless = require('serverless-http');
+const ejs = require('ejs'); // Force bundling of ejs
 const app = require('../../server');
 
 // Wrap Express app with serverless-http
